302ec32bddb5f425279a89ac6c4ff5e873f82fd4
Maciej Rek <maciej.rek.kontakt@gmail.com>
2020-04-19 11:45:52 +0200

    Custom fonts in text widget + text styling

--- 02-flutter-basics/hello_world/lib/app_screens/home.dart
+++ 02-flutter-basics/hello_world/lib/app_screens/home.dart
@@ -6,10 +6,20 @@ class Home extends StatelessWidget{
     return Center(child: Container(
       alignment:Alignment.center,
       color:Colors.deepPurple,
-      // width:200.0,
-      // height:100.0,
-      // margin: EdgeInsets.only(left:15.0,top:30.0),
-      child: Text("Flight",textDirection: TextDirection.ltr,)),);
+
+      child: Text(
+        "Flight",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          decoration:TextDecoration.none,
+          fontSize:100.0,
+          fontFamily: 'ComicNeue',
+          fontWeight: FontWeight.w300,
+          fontStyle: FontStyle.italic,
+          color:Colors.white,
+
+        ),
+        )),);
   }
 
 }
