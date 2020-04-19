1f48bc315302f1d811712f0836c220eb17c1e376
Maciej Rek <maciej.rek.kontakt@gmail.com>
2020-04-19 11:58:21 +0200

    Row,Column and Expanded widget

--- 02-flutter-basics/hello_world/lib/app_screens/home.dart
+++ 02-flutter-basics/hello_world/lib/app_screens/home.dart
@@ -1,25 +1,76 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Container(
-      alignment:Alignment.center,
-      color:Colors.deepPurple,
-
-      child: Text(
-        "Flight",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          decoration:TextDecoration.none,
-          fontSize:100.0,
-          fontFamily: 'ComicNeue',
-          fontWeight: FontWeight.w300,
-          fontStyle: FontStyle.italic,
-          color:Colors.white,
-
-        ),
-        )),);
+    return Center(
+        child: Container(
+            padding: EdgeInsets.only(left: 10.0, top: 40.0),
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                      "Col 1 Row 1",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 35.0,
+                        fontFamily: 'ComicNeue',
+                        fontWeight: FontWeight.w300,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                      ),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Col 1 Row 2",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 15.0,
+                        fontFamily: 'ComicNeue',
+                        fontWeight: FontWeight.w300,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                      ),
+                    ))
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                      "Col 2 Row 1",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 35.0,
+                        fontFamily: 'ComicNeue',
+                        fontWeight: FontWeight.w300,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                      ),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Col 2 Row 2",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 15.0,
+                        fontFamily: 'ComicNeue',
+                        fontWeight: FontWeight.w300,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                      ),
+                    ))
+                  ],
+                )
+              ],
+            )));
   }
-
 }
