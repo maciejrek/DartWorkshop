25118ada836a17ae2bb6a5241ff5648b62557c02
Maciej Rek <maciej.rek.kontakt@gmail.com>
2020-04-19 15:56:04 +0200

    First stateful widget

--- 02-flutter-basics/section_03/lib/main.dart
+++ 02-flutter-basics/section_03/lib/main.dart
@@ -1,3 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(null);
+void main() => runApp(MaterialApp(
+      title: "Stateful App Example",
+      home: FavouriteCity(),
+    ));
+
+//this class is immutable
+class FavouriteCity extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _FavouriteCityState();
+  }
+}
+
+//but the state is mutable 
+class _FavouriteCityState extends State<FavouriteCity> {
+  String nameCity = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Stateful app example")),
+        body: Container(
+          margin: EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                onSubmitted: (String userInput) => setState(()=> nameCity = userInput),
+              ),
+               TextField(
+                onChanged: (String userInput) => setState(()=> nameCity = userInput),
+              ),
+              Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Text(
+                    "Your city name is $nameCity",
+                    style: TextStyle(fontSize: 20.0),
+                  )),
+                  
+            ],
+          ),
+        ));
+  }
+}
