c14636287784347a4fc1eb7e345bb8142f6c45d8
Maciej Rek <maciej.rek.kontakt@gmail.com>
2020-04-19 11:30:26 +0200

    Container widget + margin and padding

--- 02-flutter-basics/hello_world/lib/main.dart
+++ 02-flutter-basics/hello_world/lib/main.dart
@@ -1,20 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/app_screens/first_screen.dart';
 
-void main() => runApp(MyFlutterApp());
+import 'app_screens/home.dart';
 
-class MyFlutterApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-        title: "My Flutter App",
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("My First App Screen"),
-          ),
-          body: FirstScreen()
-        )
-      );
-  }
-}
+
+void main() => runApp(MaterialApp(
+  title:"Exploring UI widgets",
+  home: Home(),
+));
