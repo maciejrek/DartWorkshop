c1f23ed27b567100ed55a44c3fa5fc25c6015a1a
Maciej Rek <maciej.rek.kontakt@gmail.com>
2020-04-19 14:11:49 +0200

    Basic list view

--- 02-flutter-basics/hello_world/lib/main.dart
+++ 02-flutter-basics/hello_world/lib/main.dart
@@ -5,5 +5,39 @@ import 'app_screens/home.dart';
 
 void main() => runApp(MaterialApp(
   title:"Exploring UI widgets",
-  home: Home(),
+  home: Scaffold(
+    appBar:AppBar(title:Text("Basic list view - meant only for few list items")),
+    body:getListView(),
+  ),
 ));
+
+
+Widget getListView(){
+  var listView = ListView(
+    children: <Widget>[
+      ListTile(
+        leading:Icon(Icons.landscape),
+        title: Text("Title text"),
+        subtitle:Text("Subtitle text"),
+        trailing: Icon(Icons.wb_sunny),
+        onTap:()=>debugPrint("Text"),
+
+      ),
+      ListTile(
+        leading:Icon(Icons.laptop_chromebook),
+        title: Text("laptop"),
+      ),
+      ListTile(
+        leading:Icon(Icons.phone_android),
+        title: Text("phone"),
+      ),
+      ListTile(
+        leading:Icon(Icons.laptop_mac),
+        title: Text("laptop mac"),
+      ),
+      Text("We can add any widget as a list element, like text"),
+      Container(color:Colors.red,height:50.0,child:Text("Or even container"))
+    ],
+  );
+  return listView;
+}
