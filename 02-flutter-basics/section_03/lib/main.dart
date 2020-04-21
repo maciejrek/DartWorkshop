5a5dc87ca737ff666393c1892fa998fe21e4db27
Maciej Rek <maciej.rek.kontakt@gmail.com>
2020-04-21 12:17:14 +0200

    Add logic to buttons and fields

--- 02-flutter-basics/section_03/lib/main.dart
+++ 02-flutter-basics/section_03/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() { 
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Simple Interest Calculator App',
       home: SIForm(),
@@ -9,7 +11,9 @@ void main() => runApp(MaterialApp(
         primaryColor: Colors.indigo,
         accentColor: Colors.indigoAccent,
       ),
-    ));
+    )
+    );
+    }
 
 class SIForm extends StatefulWidget {
   @override
@@ -21,6 +25,19 @@ class SIForm extends StatefulWidget {
 class _SIFormState extends State<SIForm> {
   var _currencies = ["Rupees", "Dollars", "Euro"];
   final _minimalPadding = 5.0;
+  var _currentItemSelected = '';
+
+  @override
+  void initState(){
+    super.initState();
+    _currentItemSelected = _currencies[0];
+  }
+
+  TextEditingController principalController = TextEditingController();
+  TextEditingController roiController = TextEditingController();
+  TextEditingController termController = TextEditingController();
+
+  var _displayResult = '';
 
   @override
   Widget build(BuildContext context) {
@@ -34,12 +51,14 @@ class _SIFormState extends State<SIForm> {
           child: ListView(
             children: <Widget>[
               getImageAsset(),
+
               Padding(
                   padding: EdgeInsets.only(
                       top: _minimalPadding, bottom: _minimalPadding),
                   child: TextField(
                     keyboardType: TextInputType.number,
                     style: textStyle,
+                    controller:principalController,
                     decoration: InputDecoration(
                         labelText: 'Principal',
                         hintText: 'Enter Principal e.g. 12000',
@@ -47,12 +66,14 @@ class _SIFormState extends State<SIForm> {
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(5.0))),
                   )),
+
               Padding(
                   padding: EdgeInsets.only(
                       top: _minimalPadding, bottom: _minimalPadding),
                   child: TextField(
                     keyboardType: TextInputType.number,
                     style: textStyle,
+                    controller: roiController,
                     decoration: InputDecoration(
                         labelText: 'Rate of Interest',
                         labelStyle: textStyle,
@@ -60,15 +81,18 @@ class _SIFormState extends State<SIForm> {
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(5.0))),
                   )),
+
               Padding(
                   padding: EdgeInsets.only(
                       top: _minimalPadding, bottom: _minimalPadding),
                   child: Row(
                     children: <Widget>[
+
                       Expanded(
                           child: TextField(
                         keyboardType: TextInputType.number,
                         style: textStyle,
+                        controller: termController,
                         decoration: InputDecoration(
                             labelText: 'Term',
                             hintText: 'Time in years',
@@ -76,39 +100,51 @@ class _SIFormState extends State<SIForm> {
                             border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(5.0))),
                       )),
+
                       Container(
                         width: _minimalPadding * 5,
                       ),
+
                       Expanded(
                           child: DropdownButton<String>(
                         items: _currencies
-                            .map((String value) => DropdownMenuItem(
-                                value: value, child: Text(value)))
+                            .map((String value) { return DropdownMenuItem(
+                                value: value, child: Text(value));})
                             .toList(),
-                        value: "Rupees",
-                        onChanged: (String newValueSelected) => null,
+                        value: _currentItemSelected,
+                        onChanged: (String newValueSelected){
+                            _onDropDownItemSelected(newValueSelected);},
                       ))
                     ],
                   )),
+
               Padding(
                   padding: EdgeInsets.only(
                       top: _minimalPadding, bottom: _minimalPadding),
-                  child: Row(
-                    children: <Widget>[
+                  child: Row(children: <Widget>[
+
                       Expanded(
                         child: RaisedButton(
                           color: Theme.of(context).accentColor,
                           textColor: Theme.of(context).primaryColorDark,
-                          child: Text("Calculate",textScaleFactor: 1.5 ,),
-                          onPressed: () {},
+                          child: Text(
+                            "Calculate",
+                            textScaleFactor: 1.5,
+                          ),
+                          onPressed: () {
+                            setState((){this._displayResult=_calculateTotalReturn();});
+                          },
                         ),
                       ),
                       Expanded(
                         child: RaisedButton(
                           color: Theme.of(context).primaryColorDark,
                           textColor: Theme.of(context).primaryColorLight,
-                          child: Text("Reset",textScaleFactor: 1.5 ,),
-                          onPressed: () {},
+                          child: Text(
+                            "Reset",
+                            textScaleFactor: 1.5,
+                          ),
+                          onPressed: () { setState((){_reset();});},
                         ),
                       )
                     ],
@@ -116,7 +152,7 @@ class _SIFormState extends State<SIForm> {
               Padding(
                 padding: EdgeInsets.only(
                     top: _minimalPadding, bottom: _minimalPadding),
-                child: Text("Todo text", style: textStyle),
+                child: Text(this._displayResult, style: textStyle),
               )
             ],
           )),
@@ -128,5 +164,29 @@ class _SIFormState extends State<SIForm> {
     Image image = Image(image: assetImage, width: 125.0, height: 125.0);
     return Container(
         child: image, margin: EdgeInsets.all(_minimalPadding * 10));
+  }
+
+  void _onDropDownItemSelected(String newValueSelected) {
+    setState(() => this._currentItemSelected = newValueSelected);
+  }
+
+  String _calculateTotalReturn()
+  {
+    double principal = double.parse(principalController.text);
+    double roi = double.parse(roiController.text);
+    double term = double.parse(termController.text);
+
+    double totalAmountPayable = principal + (principal * roi * term ) /100;
+
+    String result = 'After $term years, your investment wil lbe worth $totalAmountPayable $_currentItemSelected';
+    return result;
+  }
+
+  void _reset(){
+    principalController.text='';
+    roiController.text='';
+    termController.text='';
+    _displayResult='';
+    _currentItemSelected=_currencies[0];
   }
 }
